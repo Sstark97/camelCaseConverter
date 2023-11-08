4972095fb1a84b1f29e7616048b6f95d41fbc9af
Sstark97 <aitor@leanmind.es>
2023-11-08 19:56:00 +0000

    Test(:rotating_light:): CamelCaseConverter should transform to PascalCase a single word that not have this format

--- src/test/camelCaseConverter.test.ts
+++ src/test/camelCaseConverter.test.ts
@@ -2,6 +2,7 @@ import {camelCaseConverter} from "../core/camelCaseConverter";
 
 /**
  * "Foo" ⇒ "Foo"
+ * "foo" -> "Foo"
  * "Foo Bar" ⇒ "FooBar"
  * "Foo_Bar-Foo" ⇒ "FooBarFoo"
  */
@@ -13,4 +14,8 @@ describe("CamelCaseConverter", () => {
     it("should be the same when the text is already in PascalCase", () => {
         expect(camelCaseConverter("Foo")).toBe("Foo")
     });
+
+    it("should transform to PascalCase a single word that not have this format", () => {
+        expect(camelCaseConverter("foo")).toBe("Foo")
+    })
 })
