51f19033e05a68db4f0282cebfe7cdad6faf0a45
Sstark97 <aitor@leanmind.es>
2023-11-08 19:51:20 +0000

    Test(:rotating_light:): CamelCaseConverter should be the same when the text is already in PascalCase

--- src/test/camelCaseConverter.test.ts
+++ src/test/camelCaseConverter.test.ts
@@ -1,7 +1,16 @@
 import {camelCaseConverter} from "../core/camelCaseConverter";
 
+/**
+ * "Foo" ⇒ "Foo"
+ * "Foo Bar" ⇒ "FooBar"
+ * "Foo_Bar-Foo" ⇒ "FooBarFoo"
+ */
 describe("CamelCaseConverter", () => {
     it("should allow empty text", () => {
         expect(camelCaseConverter("")).toBe("")
     })
+
+    it("should be the same when the text is already in PascalCase", () => {
+        expect(camelCaseConverter("Foo")).toBe("Foo")
+    });
 })
