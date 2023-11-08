e593053dbb06503cdbdf3dd0e8ddd4415eacd28c
Sstark97 <aitor@leanmind.es>
2023-11-08 19:42:48 +0000

    Test(:rotating_light:): CamelCaseConvert should allow empty text

---
+++ src/test/camelCaseConverter.test.ts
@@ -0,0 +1,11 @@
+import { saidHello} from "../core/example";
+
+function camelCaseConverter(aText: string) {
+    return undefined;
+}
+
+describe("CamelCaseConverter", () => {
+    it("should allow empty text", () => {
+        expect(camelCaseConverter("")).toBe("")
+    })
+})
